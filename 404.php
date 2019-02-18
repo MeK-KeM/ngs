cdfc68343c8a8dc69111dec9d39543e091178946
devngs <info@1devserver.co.uk>
2019-02-18 20:32:25 +0500

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+	<div class="intro">
+        <div>
+            <div class="container">
+                <div class="intro-header text-center">
+                    <div class="intro-title">
+                        404
+                     </div>
+                    <div class="intro-text">
+	 					<p>Oops, something went wrong!</p>
+						<p>This page has been deleted or never existed.</p>
+						<a href="<?php bloginfo('url'); ?>" class="btn btn-warning">Go to Home Page</a>
+                    </div>
+                </div>
+                <!--.intro-header-->
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
+		
+
